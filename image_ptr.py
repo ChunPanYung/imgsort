c47625010322d1a43de2a1dde0cb3b913555967a
ChunPanYung <>
2019-07-07 18:50:51 -0400

    fix spacing problem.

--- image_ptr.py
+++ image_ptr.py
@@ -1,55 +1,56 @@
- 
 from PIL import Image
 
 class ImagePtr:
 
-  # static class variables
-  _lst = [] # contains list of object ImagePtr
+    # static class variables
+    _lst = [] # contains list of object ImagePtr
 
-  def __init__(self, width, height, ptr):
-    self.width = width
-    self.height = height
-    self.ptr = []
-    self.ptr.extend(ptr)
+    def __init__(self, width, height, ptr):
+        self.width = width
+        self.height = height
+        self.ptr = []
+        self.ptr.extend(ptr)
+        self.num = 1
 
-  def get_width(self):
-    return self.width
+    def get_width(self):
+        return self.width
 
-  def get_height(self):
-    return self.height
+    def get_height(self):
+        return self.height
 
-  def add_list(self, ptr):
-    self.ptr.extend(ptr)
+    def add_list(self, ptr):
+        self.ptr.extend(ptr)
+        self.num += 1
 
-  # Static function
-  # append image files into static class variable _lst
-  # if they are image files
-  @staticmethod
-  def append(files):
-    # check whether each file is img file, 
-    # then get the width and height
-    # put them in a list
-    for f in files:
-      img = Image.open(f)
-      try: # verify whether it's image (loose verifcation)
-        img.verify()
-        # check if it's ImagePtr already exist
-        length = len(ImagePtr._lst)
-        if length != 0:
-          # check each ImagePtr to see whether it matches the width and height
-          for l in ImagePtr._lst:
-            # if it's the same width and height
-            if img.size == (l.get_width, l.get_height):
-              l.add_list(f)
-            # Otherwise create a new ImagePtr object on the list
-            else:
-              ImagePtr._append(f)
-        # Otherwise create a new ImagePtr object on the list
-        else:
-          ImagePtr._append(f)
-      except Exception:
-        pass
-  
-  @staticmethod
-  def _append(image):
-    ImagePtr._lst.extend(ImagePtr(image.get_width, image.get_height, image))
+    # Static function
+    # append image files into static class variable _lst
+    # if they are image files
+    @staticmethod
+    def append(files):
+        # check whether each file is img file, 
+        # then get the width and height
+        # put them in a list
+        for f in files:
+            img = Image.open(f)
+            try: # verify whether it's image (loose verifcation)
+                img.verify()
+                # check if it's ImagePtr already exist
+                length = len(ImagePtr._lst)
+                if length != 0:
+                    # check each ImagePtr to see whether it matches the width and height
+                    for l in ImagePtr._lst:
+                        # if it's the same width and height
+                        if img.size == (l.get_width, l.get_height):
+                            l.add_list(f)
+                        # Otherwise create a new ImagePtr object on the list
+                        else:
+                            ImagePtr._append(f)
+                # Otherwise create a new ImagePtr object on the list
+                else:
+                    ImagePtr._append(f)
+            except Exception:
+                pass
+    
+    @staticmethod
+    def _append(image):
+        ImagePtr._lst.extend(ImagePtr(image.get_width, image.get_height, image))
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+#!/bin/python3
 import sys
 import os
 from image_ptr import ImagePtr
 from PIL import Image
 
 def main():
-  # Check to see if it's directory
-  if os.path.isdir(sys.argv[1]):
-    # get all files in the current directory
-    files = [f for f in os.listdir('.') if os.path.isfile(f)]
-    # append each image files into list
-    ImagePtr.append(files)
+    # Check to see if it's directory
+    if os.path.isdir(sys.argv[1]):
+        # get all files in the current directory
+        files = [f for f in os.listdir('.') if os.path.isfile(f)]
+        # append each image files into list
+        ImagePtr.append(files)
  
