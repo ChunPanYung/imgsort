f779db57598222f2a5c683f01c056d2f5ac56ab9
ChunPanYung <>
2020-02-20 16:02:23 -0500

    Load File dialog added.

--- gui_app.py
+++ gui_app.py
@@ -1,16 +1,32 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
-
+from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
 
-class MainWindow(Widget):
+class MainWindow(BoxLayout):
     pass
 
-class GuiApp(App):
+    def dismiss_popup(self):
+        self._popup.dismiss()
 
+    def show_load(self):
+        content: LoadDialog = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup: Popup = Popup(title="Load file", content=content,
+                                   size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.ids.selected_files.text = stream.read()
+
+class LoadDialog(FloatLayout):
+    load: ObjectProperty = ObjectProperty(None)
+    cancel: ObjectProperty = ObjectProperty(None)
+
+class GuiApp(App):
     def build(self):
         return MainWindow()
+
+
