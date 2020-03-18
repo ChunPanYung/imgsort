e44e184a2ed5e52c51913fce455239a6d8686f9c
ChunPanYung <>
2020-03-18 18:58:57 -0400

    refactoring classes

--- gui_app.py
+++ gui_app.py
@@ -2,43 +2,10 @@
 import os
 from typing import List
 
-from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.uix.popup import Popup
-from dialog_app import LoadDialog, OptionDialog
-
-from bool_collection import BoolCollection
-
-
-class BaseApp(Widget):
-    """ basic class method that will be shared among widgets """
-    def __init__(self):
-        """ initialization """
-        self._popup: Popup = None
-        self._files: List[str] = None
-        self._destination: str = None
-        self._bool_value: BoolCollection = None
-        self._limit_size: List[str] = None
-        super(BaseApp, self).__init__()
-
-    def dismiss_popup(self):
-        """ remove/dismiss gui window """
-        self._popup.dismiss()
-
-    def load_file(self, _path, filename):
-        """ empty method """
-
-    def show_load(self, _bool: bool):
-        """ display load dialog, assign function to load button and cancel button """
-        _content: LoadDialog = LoadDialog(self.load_file, self.dismiss_popup)
-        self._popup = Popup(title="Load file", content=_content,
-                            size_hint=(0.9, 0.9))
-
-        # disable multiselect: only one destination folder
-        _content.is_multiselect(_bool)
-
-        self._popup.open()
-
+from base_app import BaseApp
+from option_dialog import OptionDialog
 
 
 class MainWindow(BaseApp):
