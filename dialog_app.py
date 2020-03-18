73983527aab6b6da1013a176ce0fa5a044aef726
ChunPanYung <>
2020-03-18 18:35:18 -0400

    add methods and variable to LoadDialog and OptionDialog

--- dialog_app.py
+++ dialog_app.py
@@ -12,11 +12,21 @@ class LoadDialog(FloatLayout):
         self.cancel: ObjectProperty = cancel
         super(LoadDialog, self).__init__()
 
+    def is_multiselect(self, _bool: bool) -> None:
+        """ whether to allow selecting multiple files/directories.
+            default is allowed. """
+        self.ids.filechooser.multiselect = False # disable multi select
+
+
+
 class OptionDialog(FloatLayout):
     """ Option dialog and for choosing destination directory """
-    def __init__(self, next: ObjectProperty, cancel: ObjectProperty,
-                 files: List[str]):
-        self.next: ObjectProperty = next
+    def __init__(self, _next: ObjectProperty, cancel: ObjectProperty,
+                 load: ObjectProperty, files: List[str]):
+        self._next: ObjectProperty = _next
         self.cancel: ObjectProperty = cancel
+        self.load: ObjectProperty = load
+
         self.files: List[str] = files # contains list of source file path
+
         super(OptionDialog, self).__init__()
